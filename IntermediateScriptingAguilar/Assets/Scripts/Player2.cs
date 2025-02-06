34c45ee8e8b901bd423d083cc8cd250e3790ed88
Kimberly Aguilar <145504527+Moociie@users.noreply.github.com>
2025-02-06 15:23:45 -0800

    statics
    finished

---
+++ IntermediateScriptingAguilar/Assets/Scripts/Player2.cs
@@ -0,0 +1,14 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour
+{
+    public static int playerCount = 0;
+
+    void Start()
+    {
+        playerCount++;
+    }
+}
---
+++ IntermediateScriptingAguilar/Assets/Scripts/UtilitiesExample.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UtilitiesExample : MonoBehaviour
+{
+ 
+    void Start()
+    {
+        int x = Utilities.Add(5, 6);
+    }
+
+}
