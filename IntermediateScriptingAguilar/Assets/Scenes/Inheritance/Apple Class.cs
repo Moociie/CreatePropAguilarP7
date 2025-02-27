5eaa55d29e990287655623375e3ee8e0d875662f
Kimberly Aguilar <145504527+Moociie@users.noreply.github.com>
2025-02-27 14:54:29 -0800

    inheritance
    finnished

---
+++ IntermediateScriptingAguilar/Assets/Scenes/Inheritance/Apple Class.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleClass : FruitClass
+{
+    public AppleClass()
+    {
+        color = "red";
+        Debug.Log("1st Apple Connstructor Called");
+    }
+    public AppleClass(string newColor) : base(newColor)
+    {
+        Debug.Log("2nd Apple Constructor Called");
+    }
+}
