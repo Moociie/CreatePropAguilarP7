fe3ce7bb6b426363464531e363ec93042bda498c
Kimberly Aguilar <145504527+Moociie@users.noreply.github.com>
2025-03-11 14:10:21 -0700

    Extension Methods
    finished

---
+++ IntermediateScriptingAguilar/Assets/Scripts/Extension Methods/SomeClass3.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeClass3 : MonoBehaviour
+{
+    void Start()
+    {
+        transform.ResetTransformation();
+    }
+
+}
